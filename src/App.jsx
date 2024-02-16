4dcfe48ecd72d691212f0f35d0e505f6a86ff663
rollie <rollieminton@outlook.com>
2024-02-16 18:43:12 -0500

    Solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,14 @@
+import SignUpForm from "./Components/SignUpForm";
+import Authenticate from "./Components/Authenticate";
+import { useState } from "react";
 import "./App.css";
 
-export default function App() {}
+export default function App() {
+  const [token, setToken] = useState(null);
+  return (
+    <>
+      <SignUpForm token={token} setToken={setToken} />
+      <Authenticate token={token} setToken={setToken} />
+    </>
+  );
+}
